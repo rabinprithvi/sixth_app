f70c148d07694154f73eb9f4f19037ffb8ffe8aa
Rabindranath Prithvi <rabindranath.prithvi@gmail.com>
2012-03-10 03:35:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SixthApp::Application.config.secret_token = 'b9b8725312c0a238491036947fa12bc7cd7fbe1ea48cac38fb9101e89d513bf9a766030770d87800cab266d7e5a6669c416535010cae28187afa6656a704e8e2'
